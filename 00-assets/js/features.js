a5bdcf34903ffcd45eaf70fba5b4133a2b628242
SWB-BBALL <evan.cg.cooper@outlook.com>
2026-08-08 11:41:16 -0230

    update

--- 00-assets/js/features.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  "use strict";
-
-  if (window.console && typeof window.console.warn === "function") {
-    window.console.warn("features.js is deprecated. Load core.js plus the split feature modules instead.");
-  }
-})();
